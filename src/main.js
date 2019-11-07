cfff9b19f8b502857377e128403736ca14736651
annacraven <anna.craven@yale.edu>
2019-11-07 14:22:33 -0500

    more changes from 11/5

--- src/main.js
+++ src/main.js
@@ -1,5 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+import BootstrapVue from 'bootstrap-vue'
+import VueRouter from 'vue-router'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+Vue.use(BootstrapVue);
+Vue.use(VueRouter);
 
 Vue.config.productionTip = false
 
