3517bebcc0495ce7894ded0d53043f0d62643366
annacraven <anna.craven@yale.edu>
2019-11-12 14:24:47 -0500

    implement flow of app

---
+++ src/components/imageCheck.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="container p-3">
+    <h2>Do you see this place?</h2>
+    <b-img thumbnail :src="'marker/' + marker + '.jpg'"></b-img>
+  </div>
+</template>
+
+<script>
+  export default {
+    methods: {
+      incrementPage () {
+        this.$store.dispatch('incrementActionPage', 1)
+      }
+    },
+    computed: {
+      page () {
+        return this.$store.getters.page
+      },
+      marker () {
+        return this.$store.getters.marker
+      }
+    }
+  }
+</script>
+
+<style>
+</style>
