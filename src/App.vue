f0be6268a3597cd4347996c26feae91d3f78f451
Zeynep Karacan <zeynepkaracan@MacBook-Pro-68.wireless.yale.internal>
2019-11-12 13:21:32 -0500

    Took out vue Router, everything works

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app">
+    <Map v-if="!pageType"></Map>
+    <Info v-if="pageType"></Info>
+    <p>technically changed</p>
+    <button @click="changePage()">Next</button> 
+  </div>
+</template>
+
+<script>
+import Map from './components/Map.vue'
+import Info from './components/Info.vue'
+
+export default {
+  name: 'app',
+  data: () => ({
+    pageType: 0
+  }),
+  components: {
+    Map, 
+    Info
+  }, 
+  methods: {
+    changePage: function() {
+      this.pageType = !this.pageType;
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Info.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <p>Info Goes Here</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Info',
+  props: {
+  }
+}
+</script>
