a3df8d81ef3e22b6a975898463ec63e825bc130e
Zeynep Karacan <zeynepkaracan@MacBook-Pro-68.wireless.yale.internal>
2019-12-05 13:41:37 -0500

    Restored functionality of user location tracking and changed map wrapper structure

--- src/store/index.js
+++ src/store/index.js
@@ -7,9 +7,13 @@ export const store = new Vuex.Store({
 
   state: {
     page: 0,
-    marker: 0
+    marker: 0,
+    apikey : "AIzaSyDn3JZPmqYtO73Cd4r1b-x1ClNlF0NsXf8",
+    google : Object,
+    // map : Object
   },
 
+  //action calls the mutation which actually changes the state
   mutations: {
     incrementPage (state, payload) {
       state.page = (state.page + payload) % 3
@@ -22,9 +26,17 @@ export const store = new Vuex.Store({
     },
     setMarker (state, payload) {
       state.marker = payload
-    }
+    },
+    setGoogle(state, payload) {
+      state.google = payload
+    },
+    // setMap(state, payload) {
+    //   state.map = new payload;
+    // }
   },
 
+  //what you would call in the component
+  //always do $store.dispatch('ACTION NAME') AND payload is the argument
   actions: {
     incrementActionPage ({commit}, payload) {
       commit('incrementPage', payload)
@@ -37,7 +49,13 @@ export const store = new Vuex.Store({
     },
     setActionMarker ({commit}, payload) {
       commit('setMarker', payload)
-    }
+    },
+    setActionGoogle({commit}, payload) {
+      commit('setGoogle', payload)
+    },
+    // setActionMap({commit}, payload) {
+    //   commit('setMap', payload)
+    // }
   },
 
   getters: {
@@ -46,6 +64,15 @@ export const store = new Vuex.Store({
     },
     marker (state) {
      return state.marker
+    },
+    google (state) {
+      return state.google
+    },
+    // map(state) {
+    //   return state.map
+    // },
+    apikey(state) {
+      return state.apikey
     }
   }
 })
